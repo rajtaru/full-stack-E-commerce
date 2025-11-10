0e19385d5212706f5f992f357c4c16423749c86d
rajtaru <rajtarunrajtarun01@gmail.com>
2025-11-10 22:52:38 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>E-commerce has transformed the way people shop by bringing convenience, variety, and accessibility to customers worldwide. It allows users to browse, compare, and purchase products anytime and anywhere, creating a seamless and efficient shopping experience. From clothing and accessories to electronics and essentials, e-commerce platforms connect buyers and sellers globally, making transactions faster, safer, and more reliable. With just a few clicks, customers can enjoy doorstep delivery and a wide range of choices, reflecting the perfect blend of technology, comfort, and modern lifestyle.</p>
+        <p>From fashion to electronics, e-commerce brings everything you need to your doorstep with just a few clicks</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
